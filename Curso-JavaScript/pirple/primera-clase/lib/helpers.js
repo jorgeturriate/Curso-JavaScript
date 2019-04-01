bc18ccbf9bb49a4996d81faf97d97136e7cae15b
jorgeturriate <jorge_xt@hotmail.com>
2019-04-01 00:58:55 -0500

    Update helpers.js

--- Curso-JavaScript/pirple/primera-clase/lib/helpers.js
+++ Curso-JavaScript/pirple/primera-clase/lib/helpers.js
@@ -51,4 +51,61 @@ helpers.createRandomString= function(strLength){
     }
 };
 
+// Send an SMS via Twilio
+helpers.sendTwilioSms= function(phone,msg,callback){
+    //Validate parameters
+    phone= typeof(phone)=="string" && phone.trim().length==9 ? phone.trim() : false;
+    msg= typeof(msg)=="string" && msg.trim().length>0 && msg.trim().length<=1600 ? msg.trim() : false;
+    if(phone && msg){
+        //Configure the request payload
+        let payload= {
+            'From': config.twilio.fromPhone,
+            'To': '+1'+phone,
+            'Body': msg
+        };
+
+        //Stringify the payload
+        let stringPayload= querystring.stringify(payload);
+
+        //Configure the request details
+        let requestDetails= {
+            'protocol': 'https',
+            'hostname': 'api.twilio.com',
+            'method': 'POST',
+            'path': '/2010-04-01/Accounts/'+config.twilio.accountSid+'/Messages.json',
+            'auth': config.twilio.accountSid+":"+config.twilio.authToken,
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(stringPayload)
+            }
+        };
+
+        // Instantiate the request object
+        let req= https.request(requestDetails, function(res){
+            // Grab the status from of the sent request
+            let status= res.statusCode;
+            // Callback successfully if the request went through
+            if(status==200 || status==201){
+                callback(false);
+            } else {
+                callback('Status code returned was '+status);
+            }
+
+            // Bind to the error event so it doesn't get throw
+            req.on('error',function(e) {
+                callback(e);
+            });
+
+            //Add the payload
+            req.write(stringPayload);
+
+            //End the request
+            req.end();
+        });
+    } else{
+        callback("Given parameters are invalid");
+    }
+};
+
+
 module.exports= helpers;
